4773e03e0e4bd1935507778d60ec52be01c27095
unknown <mmohamedabdelm@gmail.com>
2023-07-01 08:22:55 +0300

    'f

--- code/test code.py
+++ code/test code.py
@@ -69,23 +69,37 @@
 
 
 
-def minion_game(string):
-    s=len(string)
-    vowel = 0
-    consonant = 0
-     
-    for i in range(s):
-        if string[i] in 'AEIOU':
-           vowel+=(s-i)
-        else:
-           consonant+=(s-i)
-                
-    if vowel < consonant:
-        print('Stuart ' + str(consonant))
-    elif vowel > consonant:
-        print('Kevin ' + str(vowel))
-    else:
-        print('Draw')
-        
-minion_game("Hello")
 
+# def minion_game(string):
+#     s=len(string)
+#     vowel = 0
+#     consonant = 0
+     
+#     for i in range(s):
+#         if string[i] in 'AEIOU':
+#            vowel+=(s-i)
+#         else:
+#            consonant+=(s-i)
+                
+#     if vowel < consonant:
+#         print('Stuart ' + str(consonant))
+#     elif vowel > consonant:
+#         print('Kevin ' + str(vowel))
+#     else:
+#         print('Draw')
+
+# minion_game("Hello")
+
+
+
+
+from itertools import product
+a = input("entera: ") 
+b = input("enterb: ") 
+a= a.split()
+b= b.split()
+
+
+for i in a:
+    for ii in b:
+        print(f" ({i},{ii})" ,end='')
